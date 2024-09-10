716f23feb79d490f1cd065f72ed4ef93d7f133c9
Luca Palmarucci <lpalmarucci@gmail.com>
2024-09-10 17:50:06 +0200

    (fix): theme in auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/context/ThemeProvider.context";
 
 export const metadata = {
   title: "Spizzichouse",
@@ -14,7 +15,9 @@ export default function RootLayut({ children }: { children: React.ReactNode }) {
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
-          <div className="w-full flex justify-center items-center min-h-screen">{children}</div>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <div className="w-full flex justify-center items-center min-h-screen">{children}</div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
